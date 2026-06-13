abe088f2116784317f107f1335db06f1418c54e1
F1rrock <flyrock000@gmail.com>
2026-06-13 16:23:05 +0800

    bootstrap has been modified, now it can initialize project

--- RePackage/R/Bootstrap.R
+++ RePackage/R/Bootstrap.R
@@ -1,7 +1,46 @@
+rejudge.project.file <- function(path) {
+  existing <- list.files(
+    path,
+    pattern = "\\.Rproj$",
+    full.names = TRUE
+  )
+  if (length(existing) > 0) {
+    return(normalizePath(existing[[1]], winslash = "/", mustWork = TRUE))
+  }
+  name <- paste0(
+    basename(normalizePath(path, winslash = "/", mustWork = TRUE)),
+    ".Rproj"
+  )
+  file <- file.path(path, project_name)
+  writeLines(
+    c(
+      "Version: 1.0",
+      "",
+      "RestoreWorkspace: Default",
+      "SaveWorkspace: Default",
+      "AlwaysSaveHistory: Default",
+      "",
+      "EnableCodeIndexing: Yes",
+      "UseSpacesForTab: Yes",
+      "NumSpacesForTab: 2",
+      "Encoding: UTF-8",
+      "",
+      "RnwWeave: Sweave",
+      "LaTeX: pdfLaTeX"
+    ),
+    file
+  )
+  normalizePath(file, winslash = "/", mustWork = TRUE)
+}
+
 rejudge.project <- function() {
   project <- rstudioapi::getActiveProject()
-  if (is.null(project) || identical(project, "")) stop("RStudio project is not open")
-  project
+  if (!is.null(project) && !identical(project, "")) {
+    return(normalizePath(project, winslash = "/", mustWork = TRUE))
+  }
+  path <- normalizePath(getwd(), winslash = "/", mustWork = TRUE)
+  rejudge.project.file(path)
+  path
 }
 
 rejudge.manifest <- function() {
@@ -11,6 +50,7 @@ rejudge.manifest <- function() {
 rejudge.load <- function() {
   old <- getwd()
   on.exit(setwd(old), add = TRUE)
+  
   setwd(rejudge.project())
   source(rejudge.manifest(), local = .GlobalEnv)
 }
