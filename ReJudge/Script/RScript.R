83f46bdd89f451b17f812257fe184eea2c3ee8cc
F1rrock <flyrock000@gmail.com>
2026-06-27 18:49:48 +0800

    module has been migrated to "stdin" writing with context instead of "input"

--- ReJudge/Script/RScript.R
+++ ReJudge/Script/RScript.R
@@ -1,25 +1,28 @@
 source("ReJudge/Script/Script.R")
 source("ReJudge/Text/Text.R")
+source("ReJudge/Script/Context/Context.R")
 
-script.r <- function(path, input) {
-  structure(list(path = path, input = input), class = "script_r")
+script.r <- function(path, input, ctx) {
+  structure(list(path = path, input = input, context = ctx), class = "script_r")
 }
 
 result.script_r <- function(x) {
   path  <- contents(x$path)
   input <- contents(x$input)
-  rejudge   <- readLines(path, warn = FALSE)
-  if (!any(grepl('file\\s*=\\s*"stdin"', rejudge))) {
-    tmp <- tempfile(fileext = ".R")
-    on.exit(unlink(tmp))
-    writeLines(
-      c('scan <- function(...) base::scan(file = "stdin", ...)', rejudge),
-      tmp
-    )
-    path <- tmp
-  }
-  paste(
-    system2("Rscript", args = path, input = input, stdout = TRUE),
-    collapse = "\n"
+  stdin <- tempfile(fileext = ".txt")
+  writeLines(input, stdin)
+  on.exit(unlink(stdin), add = TRUE)
+  tmp <- tempfile(fileext = ".R")
+  ctx <- core(x$context)
+  origin <- readLines(path, warn = FALSE)
+  writeLines(c(ctx, origin), tmp)
+  on.exit(unlink(tmp), add = TRUE)
+  result <- system2(
+    "Rscript",
+    args = c("--vanilla", tmp),
+    stdin = stdin,
+    stdout = TRUE,
+    stderr = FALSE
   )
+  paste(result, collapse = "\n")
 }
