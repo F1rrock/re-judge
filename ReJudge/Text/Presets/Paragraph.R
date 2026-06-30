374be86869b0a5ecea85e20feadd047ec1e1e982
F1rrock <flyrock000@gmail.com>
2026-06-30 23:48:21 +0800

    type of text.paragraph has been changed to function(text) instead of text

--- ReJudge/Text/Presets/Paragraph.R
+++ ReJudge/Text/Presets/Paragraph.R
@@ -1,17 +1,14 @@
-source("ReJudge/Text/Lambda.R")
 source("ReJudge/Text/Default.R")
 source("ReJudge/Text/Bind.R")
 source("ReJudge/Text/NonEmpty.R")
 source("ReJudge/Text/Fstring.R")
 
-text.paragraph <- text.lambda(
-  function(text) {
-    text.default(
-      fallback = "",
-      text.bind(
-        text.nonempty(text),
-        function(x) text.fstring("%s\n", x)
-      )
+text.paragraph <- function(text) {
+  text.default(
+    fallback = "",
+    text.bind(
+      text.nonempty(text),
+      function(x) text.fstring("%s\n", x)
     )
-  }
-)
+  )
+}
