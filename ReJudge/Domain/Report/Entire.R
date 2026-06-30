68fe8b59b90a410cfab01e453889ee99ba0af438
F1rrock <flyrock000@gmail.com>
2026-06-30 23:31:30 +0800

    now can display the entire text of the report

---
+++ ReJudge/Domain/Report/Entire.R
@@ -0,0 +1,31 @@
+source("ReJudge/Text/Join.R")
+source("ReJudge/Dom/Engine/Engine.R")
+source("ReJudge/Dom/Element/Text.R")
+source("ReJudge/Collection/Map.R")
+source("ReJudge/Collection/Take.R")
+source("ReJudge/Collection/DropUntil.R")
+
+report.entire <- function(engine) {
+  dom <- dom(engine)
+  text <- element.text(dom, separator = " ")
+  function(page) {
+    text.join(
+      "\n",
+      collection.map(
+        text,
+        collection.takeuntil(
+          function(e) !dom$matches("div#footer", e),
+          collection.dropuntil(
+            function(e) dom$matches("h2", e),
+            dom$children(
+              dom$selection(
+                "div.l13", 
+                dom$root(page)
+              )
+            )
+          )
+        )
+      )
+    )
+  }
+}
