3bf3bb869e42cc1e50ca85c844a3bcd6ea59b68c
F1rrock <flyrock000@gmail.com>
2026-07-22 21:13:31 +0300

    the logic becomes easier

--- ReJudge/Text/Interpolated.R
+++ ReJudge/Text/Interpolated.R
@@ -2,18 +2,17 @@ source("ReJudge/Text/Text.R")
 source("ReJudge/Text/Lambda.R")
 
 text.interpolated <- function(template, ...) {
-  text.lambda(
-    function() {
-      vals <- lapply(list(...), contents)
-      template <- contents(template)
-      for (nm in names(vals)) {
-        template <- gsub(
-          sprintf("`@@`\\(%s\\)", nm),
-          vals[[nm]],
-          template
-        )
-      }
-      template
+  text.lambda(function() {
+    vals <- lapply(list(...), contents)
+    template <- contents(template)
+    for (nm in names(vals)) {
+      template <- gsub(
+        paste0("`@@`(", nm, ")"),
+        vals[[nm]],
+        template,
+        fixed = TRUE
+      )
     }
-  )
+    template
+  })
 }
